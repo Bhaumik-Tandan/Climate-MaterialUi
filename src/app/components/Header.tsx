098a2ea335915c41a9551745af426e4a217c4f8e
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-04-22 12:28:48 +0530

    [add] horizontal and veritcal padding

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,12 +3,15 @@ import { AppBar, Toolbar, InputBase, IconButton, Typography, Box } from '@mui/ma
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Image from 'next/image';
+import Group from "../assets/Group.png";
 
 const Header = () => {
   return (
     <AppBar position="static" color="default" elevation={0}>
-      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider', justifyContent: 'space-between' }}>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider', justifyContent: 'space-between', py: 2 }}> 
+        <Box sx={{ display: 'flex', alignItems: 'center', px: 3 }}> 
+            <Image src={Group} alt="Climate Angels" width="48.45" height="48" />
           <Typography variant="h6" color="inherit" noWrap>
             Climate Angels
           </Typography>
@@ -22,13 +25,14 @@ const Header = () => {
             placeholder="Search by company name..."
             sx={{
               color: 'inherit',
-              pl: `calc(1em + 32px)`,
+              pl: `calc(1em + 32px)`, // Horizontal padding is included here in the calculation for the input padding
+              pr: 1, // Added horizontal padding to the right side
               width: '100%',
             }}
           />
         </Box>
 
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', px: 1 }}> {/* Added horizontal padding here */}
           <IconButton color="inherit">
             <LockOutlinedIcon />
           </IconButton>
