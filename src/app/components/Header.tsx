cf29180ab63d7d0c70d7a1dc80e4e2d4524ad6e7
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-04-22 13:20:44 +0530

    [add] search

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,32 +5,39 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Image from 'next/image';
 import Group from "../assets/Group.png";
+import Search from "../assets/Search.png";
 
 const Header = () => {
   return (
-    <AppBar position="static" color="default" elevation={0}>
+    <AppBar position="static" color="default" elevation={0} 
+    sx={{ backgroundColor: '#ffffff' }}
+    >
       <Toolbar sx={{ borderBottom: 1, borderColor: 'divider', justifyContent: 'space-between', py: 2 }}> 
         <Box sx={{ display: 'flex', alignItems: 'center', px: 3 }}> 
-            <Image src={Group} alt="Climate Angels" width="48.45" height="48" />
-          <Typography variant="h6" color="inherit" noWrap>
+            <Image src={Group} alt="Climate Angels" width="48.45" height="48"
+            style={{ paddingRight: '10px' }}
+             />
+          <Typography variant="h6" color="#4C4C4C" noWrap>
             Climate Angels
           </Typography>
         </Box>
 
-        <Box sx={{ position: 'relative', ml: 1, flex: 1 }}>
-          <Box sx={{ position: 'absolute', left: 0, top: 0, bottom: 0, display: 'flex', alignItems: 'center', pl: 2 }}>
-            <SearchIcon />
-          </Box>
-          <InputBase
-            placeholder="Search by company name..."
-            sx={{
-              color: 'inherit',
-              pl: `calc(1em + 32px)`, // Horizontal padding is included here in the calculation for the input padding
-              pr: 1, // Added horizontal padding to the right side
-              width: '100%',
-            }}
-          />
-        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center',     width: '320px',
+    padding: '10px',
+    border: '1px solid #C0C0C0',
+    borderRadius: '8px' }}> 
+      <Image
+        alt="search"
+        src={Search}
+        width="20"
+        height="20"
+      />
+      <InputBase
+        sx={{ ml: 1, flex: 1, width: 300 }}  
+        placeholder="Search by company name..."
+      />
+    </Box>
+          
 
         <Box sx={{ display: 'flex', alignItems: 'center', px: 1 }}> {/* Added horizontal padding here */}
           <IconButton color="inherit">
